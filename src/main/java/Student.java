e76e63f460564d9b5db80fb65ae6f6264532431d
Dave Dykes <148535937+brightworkdigital@users.noreply.github.com>
2026-05-04 16:38:02 +0100

    added comments for instance and static fields.

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,5 +1,5 @@
 public class Student {
-    // These are instance variables.  So called because every instance gets a copy of them.
+    // These are instance variables.  So called because every instance gets its own copy of them.
     // It wouldn't be very useful if every student object we create is named Ahmad.
     // (and, if every student's name was Ahmad, I'd screw up the pronunciation of everyone!)
     private String name;
@@ -7,6 +7,8 @@ public class Student {
 
     // Static field is the same for all instances.
     // You can think of it kinda like a class variable.  If you change it, you change it for all instances of the class.
+    // So, what's it good for?  It's only useful when all instance should have the same value.
+    // Every student has their own name, but they all study at the same institution.
     public static String institution = "YUU";
 
     public Student(String name, int commits) {
@@ -18,7 +20,7 @@ public class Student {
     }
 
     // Method below cannot be static.  Why?  Because it accesses an instance variable and there is no instance...
-    // There is no "this" (and there is no spoon)
+    // There is no "this" which is how we access the particular instance we're "in" (and there is no spoon)
     public void introduce() {
         System.out.println("Hi, my name is " + this.name);
     }
